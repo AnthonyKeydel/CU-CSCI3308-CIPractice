782e032b9432668b3dbd2c4a8f31668e469381df
AnthonyKeydel <Anke5631@colorado.edu>
2018-04-16 19:24:13 -0600

    complete?

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,22 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+START_TEST(test_2d_area_triangle){
+	coord_2d_t a;
+	coord_2d_t b;
+	coord_2d_t c;
+
+	a.x=-3;
+	a.y=2;
+	b.x=24;
+	b.y=-3.8;
+	c.x=666;
+	c.y=69;
+	ck_assert(coord_2d_area_triangle(&a, &b, &c) == 2844.6);
+
+}
+END_TEST
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,10 +178,15 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
+
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area_triangle);
 
     /* Return Suite */
     return s;
@@ -181,7 +202,6 @@ int main(void){
     srunner_run_all(sr, CK_VERBOSE);
     failed = srunner_ntests_failed(sr);
     srunner_free(sr);
-
     return (failed ? EXIT_FAILURE : EXIT_SUCCESS);
 
 }
